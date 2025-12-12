15e4d142a840f6ce89c30712cdbeed620b11064f
Lior <lior31197@gmail.com>
2025-12-12 05:21:26 +0200

    Move orders icon to right and green cart hover

--- app/components/Header.js
+++ app/components/Header.js
@@ -106,14 +106,7 @@ export default function Header() {
                             </svg>
                         </Link>
 
-                        <SignedIn>
-                            <Link href="/orders" className="relative group relative">
-                                <span className="sr-only">ההזמנות שלי</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 group-hover:text-blue-600 transition">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.05 1.557M6.75 21.75H4.875c-.621 0-1.125-.504-1.125-1.125V3.225c0-.621.504-1.125 1.125-1.125h1.562a2.31 2.31 0 010 1.5H6.75v18.125z" />
-                                </svg>
-                            </Link>
-                        </SignedIn>
+
 
                         <Link href="/cart" className="relative group">
                             <span className="sr-only">עגלה</span>
@@ -126,7 +119,7 @@ export default function Header() {
                                 strokeWidth="1.5"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
-                                className="group-hover:text-gray-600 transition"
+                                className="group-hover:text-green-600 transition"
                             >
                                 <circle cx="9" cy="21" r="1"></circle>
                                 <circle cx="20" cy="21" r="1"></circle>
