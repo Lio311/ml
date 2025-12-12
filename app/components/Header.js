64bc590d5caeccd92e021d0f75e594b912cd397f
Lior <lior31197@gmail.com>
2025-12-12 06:02:59 +0200

    Force update all layout and translation fixes

--- app/components/Header.js
+++ app/components/Header.js
@@ -125,18 +125,14 @@ export default function Header() {
                             <span className="sr-only">עגלה</span>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                width="22" height="22"
-                                viewBox="0 0 24 24"
                                 fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
                                 stroke="currentColor"
-                                strokeWidth="1.5"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
+                                width="22" height="22"
                                 className="group-hover:text-green-600 transition"
                             >
-                                <circle cx="9" cy="21" r="1"></circle>
-                                <circle cx="20" cy="21" r="1"></circle>
-                                <path d="M1 1h4l2.68 13.39a2 0 0 0 2 1.61h9.72a2 0 0 0 2-1.61L23 6H6"></path>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                             </svg>
                             {cartCount > 0 && (
                                 <span className="absolute -top-2 -right-2 flex h-4 w-4 items-center justify-center rounded-full bg-black text-[10px] text-white font-bold">
