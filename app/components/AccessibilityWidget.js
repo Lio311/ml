4fb52c77051dfc029bd5ee77061a469cdfd386f6
Lior <lior31197@gmail.com>
2025-12-16 15:55:11 +0200

    Fix: Accessibility Close Button Z-Index and Hover

--- app/components/AccessibilityWidget.js
+++ app/components/AccessibilityWidget.js
@@ -38,20 +38,7 @@ export default function AccessibilityWidget() {
     if (!isVisible) return null;
 
     return (
-        <div className="fixed bottom-6 left-6 z-50 font-sans" style={{ direction: 'rtl' }}>
-
-            {/* Close Button (X) - Allows hiding the widget */}
-            {!isOpen && (
-                <button
-                    onClick={(e) => { e.stopPropagation(); setIsVisible(false); }}
-                    className="absolute -top-2 -right-2 z-50 bg-gray-200 text-gray-600 rounded-full p-1 opacity-0 hover:opacity-100 transition shadow-sm hover:bg-red-500 hover:text-white"
-                    title="סגור כלי נגישות"
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                    </svg>
-                </button>
-            )}
+        <div className="fixed bottom-6 left-6 z-50 font-sans group" style={{ direction: 'rtl' }}>
 
             {/* Main Toggle Button */}
             <button
@@ -61,9 +48,22 @@ export default function AccessibilityWidget() {
                 title="תפריט נגישות"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M19 13v-2c-1.54.02-3.09-.75-4.07-1.83l-1.29-1.43c-.17-.19-.38-.34-.61-.45-.01 0-.01-.01-.01-.02-.01H13c-.35-.2-.75-.3-1.19-.26C10.76 7.11 10 8.04 10 9.09V15c0 1.1.9 2 2 2h5v5h2v-5.5c0-1.1-.9-2-2-2h-3v-3.45c1.29 1.07 3.25 1.94 5 1.95zm-6.17 5c-.41 1.16-1.52 2-2.83 2-1.66 0-3-1.34-3-3 0-1.31.84-2.41 2-2.83V12.1a5 5 0 1 0 1.22 9.3 5.53 5.53 0 0 0 4.61-2.4M12 6c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z" />
+                    <path d="M12 2c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2zm9 7h-6v13h-2v-6h-2v6H9V9H1V7h6V1.24c-2-.16-4.9-1.59-5.55-4.24l-1.9 1.9-.7-1.4L2.85.5C5.05.5 8 2.5 8 2.5V7h13v2zM12 22c-4.97 0-9-4.03-9-9s4.03-9 9-9c4.97 0 9 4.03 9 9s-4.03 9-9 9zm1.25-13.88c-.37-.08-.75-.12-1.15-.12-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6c0-2.31-1.31-4.32-3.25-5.3l-.6.52v4.9z" />
                 </svg>
             </button>
+
+            {/* Close Button (X) - Allows hiding the widget */}
+            {!isOpen && (
+                <button
+                    onClick={(e) => { e.stopPropagation(); setIsVisible(false); }}
+                    className="absolute -top-2 -right-2 z-[60] bg-gray-200 text-gray-600 rounded-full p-1 opacity-0 group-hover:opacity-100 transition shadow-sm hover:bg-red-500 hover:text-white"
+                    title="סגור כלי נגישות"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                    </svg>
+                </button>
+            )}
 
             {/* Menu */}
             {isOpen && (
