4d570e7b63790895297ff5a08a013fb0ecd87455
Lior <lior31197@gmail.com>
2026-03-13 18:12:51 +0200

    ui: move phone validation checkmark to the right

--- app/cart/CartClient.js
+++ app/cart/CartClient.js
@@ -840,7 +840,7 @@ export default function CartClient() {
                                         }}
                                     />
                                     {phoneNumber.length === 10 && !phoneError && (
-                                        <div className="absolute left-3 top-1/2 -translate-y-1/2 text-green-500">
+                                        <div className="absolute right-3 top-1/2 -translate-y-1/2 text-green-500">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                                 <path fillRule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4.001-5.5Z" clipRule="evenodd" />
                                             </svg>
