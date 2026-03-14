b6a7d01da677b95a2916bea5676c529f3a2ef3e9
Lior <lior31197@gmail.com>
2026-03-14 20:45:52 +0200

    fix: incorrect relative import path for CartContext in CatalogCartClient.js

--- app/catalog/[slug]/cart/CatalogCartClient.js
+++ app/catalog/[slug]/cart/CatalogCartClient.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCart } from "../../../../context/CartContext";
+import { useCart } from "../../../context/CartContext";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation';
