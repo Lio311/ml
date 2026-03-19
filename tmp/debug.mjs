c2f08f43d43fe6efc38858f2cb65dc43083ab980
Lior <lior31197@gmail.com>
2026-03-19 17:23:26 +0200

    Cleanup: Remove tmp files from repository

--- tmp/debug.mjs
+++
@@ -1,9 +0,0 @@
-import { clerkClient } from '@clerk/nextjs/server';
-
-async function test() {
-    try {
-        const clerk = await clerkClient();
-        console.log(Object.keys(clerk.users));
-    } catch(e) { console.log(e); }
-}
-test();
