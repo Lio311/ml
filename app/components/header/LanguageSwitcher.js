733538e559fd8dca6d34d4394423d34aa5eae021
Lior <lior31197@gmail.com>
2026-03-20 18:12:10 +0200

    Fixed import path in LanguageSwitcher.js to resolve build error

--- app/components/header/LanguageSwitcher.js
+++ app/components/header/LanguageSwitcher.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useLanguage } from "../context/LanguageContext";
+import { useLanguage } from "../../context/LanguageContext";
 import { motion } from "framer-motion";
 
 export default function LanguageSwitcher({ variant = "header" }) {
