75b593d86e1fb831980119f59e1bbe90c2f85890
Lior <lior31197@gmail.com>
2026-03-21 20:27:05 +0200

    Revert to next/image for brand logos as user fixed the source links

--- app/components/BrandInsight.js
+++ app/components/BrandInsight.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { getBrandInsight } from '../lib/db';
 import { cookies } from 'next/headers';
 import he from '../data/locales/he.json';
@@ -41,10 +42,13 @@ export default async function BrandInsight({ brand }) {
                     <div className="flex flex-col md:flex-row gap-8 items-start">
                         <div className="w-full md:w-1/3 flex justify-center md:justify-end mb-4 md:mb-0">
                             <div className="p-4 border border-gray-100 rounded-2xl bg-white shadow-sm flex items-center justify-center aspect-square w-48 h-48 md:w-64 md:h-64 sticky top-24">
-                                <img 
+                                <Image 
                                     src={insight.logo_url || 'https://images.unsplash.com/photo-1541643600914-78b084683601?q=80&w=512&h=512&auto=format&fit=crop'} 
                                     alt={insight.name} 
+                                    width={256} 
+                                    height={256} 
                                     className="w-full h-full object-contain"
+                                    priority
                                 />
                             </div>
                         </div>
