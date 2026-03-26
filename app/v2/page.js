05196344b3d6056b30812541d22a7dd1a8d8c9de
Lior <lior31197@gmail.com>
2026-03-26 19:35:56 +0200

    Finalize hero-to-stats alignment on mobile

--- app/v2/page.js
+++ app/v2/page.js
@@ -149,10 +149,9 @@ export default async function HomeV2() {
         </div>
       </section>
 
-      <div className="relative z-30 -mt-24 md:mt-0">
+      <div className="relative z-30 -mt-20 md:mt-0 pb-6 md:pb-0">
         <LiveStats stats={stats} />
       </div>
-      <div className="bg-white h-10 md:h-0" />
       <HomeClient newArrivals={newArrivals} topCatalogs={topCatalogs} />
       <BonusesSection />
       <BrandCarousel brands={stats.allBrands} />
