7f9644b36fda32b4e07cb78e241032a3d2c618ac
Lior <lior31197@gmail.com>
2026-04-10 21:05:30 +0300

    style: remove arrow icon from cart login button

--- app/catalog/[slug]/cart/CatalogCartClient.js
+++ app/catalog/[slug]/cart/CatalogCartClient.js
@@ -188,9 +188,6 @@ export default function CatalogCartClient({ slug }) {
                                 className="w-full bg-blue-600 text-white py-5 rounded-2xl font-black text-lg shadow-xl hover:bg-blue-700 transition-all active:scale-95 flex items-center justify-center gap-2"
                             >
                                 <span>התחברו כעת</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6">
-                                    <path fillRule="evenodd" d="M3 10a.75.75 0 0 1 .75-.75h10.638L10.23 5.29a.75.75 0 1 1 1.04-1.08l5.5 5.25a.75.75 0 0 1 0 1.08l-5.5 5.25a.75.75 0 1 1-1.04-1.08l4.158-3.96H3.75A.75.75 0 0 1 3 10Z" clipRule="evenodd" />
-                                </svg>
                             </button>
                         </div>
                     )}
