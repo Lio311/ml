60fa5121aff48ee0ebbab69cc75d547452a3a2da
Lior <lior31197@gmail.com>
2026-04-26 19:17:36 +0300

    UI: Unified handwriting font for English (Dancing Script) and Hebrew (Gveret Levin)

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Assistant, Dancing_Script } from "next/font/google";
+import { Assistant, Dancing_Script, Gveret_Levin } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs';
 import { heIL, enUS } from "@clerk/localizations";
 import "./globals.css";
@@ -32,6 +32,12 @@ const dancingScript = Dancing_Script({
   subsets: ["latin"],
   weight: "400",
   variable: "--font-dancing-script",
+});
+
+const gveretLevin = Gveret_Levin({
+  subsets: ["hebrew", "latin"],
+  weight: "400",
+  variable: "--font-gveret-levin",
 });
 
 export const viewport = {
@@ -115,7 +121,7 @@ export default async function RootLayout({ children }) {
       }}
     >
       <html lang={locale} dir={dir} style={{ colorScheme: 'light' }}>
-        <body className={`${assistant.variable} ${dancingScript.variable} antialiased`}>
+        <body className={`${assistant.variable} ${dancingScript.variable} ${gveretLevin.variable} antialiased`}>
           <LanguageProvider initialLocale={locale}>
             <CartProvider>
               <WishlistProvider>
