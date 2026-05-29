f01730ad77c3a22221a3262e68bdcf38d8871fcb
Lior <lior31197@gmail.com>
2026-05-29 23:22:06 +0300

    Revert hero padding layout changes

--- app/page.js
+++ app/page.js
@@ -126,7 +126,7 @@ export default async function Home() {
         }}
       />
       {/* Hero Section - Tall and pulled to top on mobile */}
-      <section className="relative h-[85vh] md:h-[82vh] w-full m-0 p-0 overflow-hidden bg-white block">
+      <section className="relative h-[85vh] md:h-[82vh] w-full m-0 p-0 overflow-hidden bg-white block !-mt-20 md:mt-0">
         <div className="absolute inset-0 w-full h-full overflow-hidden">
           <HeroCarousel 
             banners={banners} 
