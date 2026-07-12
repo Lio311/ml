d64a002a38840c6d6d73614369cda76ded845060
Lior <lior31197@gmail.com>
2026-07-12 19:29:02 +0300

    chore: remove scratch files

--- parse.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('temp_products.json', 'utf8'));
-const out = data.products.map(p => `${p.brand} - ${p.model} (${p.category})`).join('\n');
-fs.writeFileSync('temp_names.txt', out);
