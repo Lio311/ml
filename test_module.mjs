0c4a7a22b577bb2d44b532628a565cb50b0f64a5
Lior <lior31197@gmail.com>
2026-07-15 23:09:10 +0300

    chore: remove scratch files with sensitive data

--- test_module.mjs
+++
@@ -1,9 +0,0 @@
-async function test() {
-    const emailModule = await import('./app/lib/email.js');
-    console.log("From import:", Object.keys(emailModule));
-    
-    // In Next.js, require might work differently, but let's check basic Node interop
-    // const emailModuleReq = require('./app/lib/email.js');
-    // console.log("From require:", Object.keys(emailModuleReq));
-}
-test();
--- test_query_email.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config({path: '.env.local'});
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-pool.query("SELECT * FROM email_logs WHERE subject LIKE '%סיכום אימיילים יומי%' OR subject LIKE '%סיכום יומי%' ORDER BY sent_at DESC LIMIT 10")
-    .then(res => {
-        console.log('Daily summary emails:', res.rows);
-    })
-    .catch(console.error)
-    .finally(() => pool.end());
