85d7281fc10f0466a5f90a075b5926fb90fb4af6
Lior Zafrir <liorzafrir@Liors-MacBook-Pro.local>
2026-08-05 10:48:46 +0300

    add manual revalidate route

---
+++ app/api/admin/revalidate-home/route.js
@@ -0,0 +1,8 @@
+import { revalidateTag, revalidatePath } from 'next/cache';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    revalidateTag('home-data');
+    revalidatePath('/');
+    return NextResponse.json({ success: true });
+}
